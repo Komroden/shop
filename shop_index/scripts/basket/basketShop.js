d11a1b870ed4f3796146bba24a892730630ff3d2
Komroden <Komroden@gmail.com>
2021-07-01 19:13:29 +0300

    basketnew

---
+++ shop_index/scripts/basket/basketShop.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const openBasketBtn = document.querySelector('.header__carticonwrap');
+const basketEl = document.querySelector('.header__basket');
+const basketCounterEl = document.querySelector('.header__carticonwrap span');
+const basketTotalEl = document.querySelector('.header__baskettotal');
+const basketTotalValueEl = document.querySelector('.header__baskettotalvalue');
+
+openBasketBtn.addEventListener('click', function () {
+    basketEl.classList.toggle('hidden');
+});
+let basket = {};
+
+function addProductToObject(productId) {
+    if (!(productId in basket)) {
+        basket[productId] = 1;
+    } else {
+        basket[productId]++;
+    }
+}
+
+function renderProductInBasket(productId) {
+    let productExist = document.querySelector(`.productCount[data-productId="${productId}"]`);
+    if (productExist) {
+        increaseProductCount(productId);
+        recalculateSumForProduct(productId);
+    } else {
+        renderNewProductInBasket(productId);
+    }
+}
+
+function renderNewProductInBasket(productId) {
+    let productRow = `
+        <div class="basketRow">
+            <div>${products[productId].name}</div>
+            <div>
+                <span class="productCount" data-productId="${productId}">1</span> шт.
+            </div>
+            <div>$${products[productId].price}</div>
+            <div>
+                $<span class="productTotalRow" data-productId="${productId}">${products[productId].price}</span>
+            </div>
+        </div>
+    `;
+    basketTotalEl.insertAdjacentHTML("beforebegin", productRow);
+}
+
+function increaseProductCount(productId) {
+    const productCountEl = document.querySelector(`.productCount[data-productId="${productId}"]`);
+    productCountEl.textContent++;
+}
+
+function recalculateSumForProduct(productId) {
+    const productTotalRowEl = document.querySelector(`.productTotalRow[data-productId="${productId}"]`);
+    let totalPriceForRow = (basket[productId] * products[productId].price).toFixed(2);
+    productTotalRowEl.textContent = totalPriceForRow;
+}
+
+function calculateAndRenderTotalBasketSum() {
+    let totalSum = 0;
+    for (let productId in basket) {
+        totalSum += basket[productId] * products[productId].price;
+    }
+    basketTotalValueEl.textContent = totalSum.toFixed(2);
+}
+
+function increaseProductsCount() {
+    basketCounterEl.textContent++;
+}
+
+function addProductIntoBasket(productId) {
+    increaseProductsCount();
+    addProductToObject(productId);
+    renderProductInBasket(productId);
+    calculateAndRenderTotalBasketSum();
+}
